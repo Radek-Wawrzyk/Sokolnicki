dab1af3e76460ed9a0abdc5174971af97dd05994
Radek <radek511@op.pl>
2018-06-04 01:47:12 +0200

    Add - Gulp files & site structure

---
+++ gulpfile.js
@@ -0,0 +1,117 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const imagemin = require('gulp-imagemin');
+const changed = require('gulp-changed');
+const htmlReaplce = require('gulp-html-replace');
+const htmlMin = require('gulp-htmlmin');
+const del = require('del');
+const sequence = require('run-sequence');
+const babel = require('gulp-babel');
+
+const config = {
+  dist: 'dist/',
+  src: 'src/',
+  cssin: 'src/css/**/*.css',
+  jsin: 'src/js/**/*.js',
+  imgin: 'src/img/**/*.{jpg,jpeg,png,gif}',
+  htmlin: 'src/*.html',
+  scssin: 'src/scss/**/*.scss',
+  cssout: 'dist/css/',
+  jsout: 'dist/js/',
+  imgout: 'dist/img/',
+  htmlout: 'dist/',
+  scssout: 'src/css/',
+  cssoutname: 'style.css',
+  jsoutname: 'script.js',
+  cssreplaceout: 'css/style.css',
+  jsreplaceout: 'js/script.js'
+};
+
+gulp.task('reload', () => {
+  browserSync.reload();
+});
+
+gulp.task('serve', ['sass'], () => {
+  browserSync({
+    server: config.src
+  });
+
+  gulp.watch([config.htmlin, config.jsin], ['reload']);
+  gulp.watch(config.scssin, ['sass']);
+});
+
+gulp.task('sass', () => {
+  return gulp.src(config.scssin)
+    .pipe(sourcemaps.init())
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 3 versions']
+    }))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(config.scssout))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('css', () => {
+  return gulp.src(config.cssin)
+    .pipe(concat(config.cssoutname))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest(config.cssout));
+});
+
+gulp.task("js-babel", () => {
+  return gulp.src(config.jsin)
+  .pipe(sourcemaps.init())
+  .pipe(babel({
+      presets: ['env']
+  }))
+  .pipe(sourcemaps.write())
+});
+
+gulp.task('js', () => {
+  return gulp.src(config.jsin)
+    .pipe(sourcemaps.init())
+    .pipe(babel({
+      presets: ['env']
+    }))
+    .pipe(concat(config.jsoutname))
+    .pipe(uglify())
+    .pipe(gulp.dest(config.jsout));
+});
+
+gulp.task('img', () => {
+  return gulp.src(config.imgin)
+    .pipe(changed(config.imgout))
+    .pipe(imagemin())
+    .pipe(gulp.dest(config.imgout));
+});
+
+gulp.task('html', () => {
+  return gulp.src(config.htmlin)
+    .pipe(htmlReaplce({
+      'css': config.cssreplaceout,
+      'js': config.jsreplaceout
+    }))
+    .pipe(htmlMin({
+      sortAttributes: true,
+      sortClassName: true,
+      collapseWhitespace: true
+    }))
+    .pipe(gulp.dest(config.dist))
+});
+
+gulp.task('clean', () => {
+  return del([config.dist]);
+});
+
+gulp.task('build', () => {
+  sequence('clean', ['html', 'js', 'css', 'img']);
+});
+
+gulp.task('default', ['serve']);
