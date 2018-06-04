cd29b89b5c4a6618f33f7e03499fedd31174f36b
Radek <radek511@op.pl>
2018-06-04 14:59:36 +0200

    Add - Navigation with styles & Javascript
    Aria tag for better accessibility, keyboard control, animation

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,41 @@
-console.log("it works!");
+const navigation = () => {
+
+  const navMenu = document.querySelector(".navigation-right");
+  const navBtn = document.querySelector(".navigation-button");
+  const navLink = document.querySelectorAll(".navigation-menu li a");
+
+  navBtn.addEventListener("click", () => {
+    navBtn.classList.toggle("active");
+    navMenu.classList.toggle("active");
+
+    let aria = navBtn.getAttribute("aria-expanded");
+    if (aria == "false") {
+      aria = "true"
+    } else {
+      aria = "false";
+    }
+    navBtn.setAttribute("aria-expanded", aria);
+  });
+
+  const close = () => {
+    navBtn.classList.remove("active");
+    navBtn.setAttribute("aria-expanded", false);
+    navMenu.classList.remove("active");
+  };
+
+  for (let i = 0; navLink.length > i; i++) {
+    navLink[i].addEventListener("click", () => {
+      close();
+    });
+  }
+
+  document.onkeydown = (e) => {
+    e = e || window.event;
+    if (e.keyCode == 27) {
+      close();
+    }
+  };
+}
+
+//Function inits
+navigation();
